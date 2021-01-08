ef2e68e89a8757041bf58705de66591863fce442
BrainStormDevel <pendley@libero.it>
2021-01-08 11:17:54 +0100

    Update BasePathDetector.php

--- src/BasePathDetector.php
+++ src/BasePathDetector.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Selective\BasePath;
+namespace BrainStormDevel\BasePath;
 
 /**
  * A URL base path detector.
@@ -85,7 +85,7 @@ class BasePathDetector
 
         $basePath = (string)parse_url($server['REQUEST_URI'], PHP_URL_PATH);
         $scriptName = str_replace('\\', '/', dirname(dirname($scriptName)));
-		$basePath = ((substr($basePath, -1) === '/') ? substr($basePath, 0, strlen($basePath)) : $basePath);
+	$basePath = ((substr($basePath, -1) === '/') ? substr($basePath, 0, strlen($basePath)) : $basePath);
 
         if (($scriptName === '/') && ($basePath === '/')) {
             return '';
