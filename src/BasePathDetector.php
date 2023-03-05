4ac123d6327e9928e6834e4b80dfbbd46c95bda4
Davide Bonsangue <davidebonsangue@gmail.com>
2023-03-05 19:44:50 +0100

    Update BasePathDetector.php

--- src/BasePathDetector.php
+++ src/BasePathDetector.php
@@ -36,29 +36,24 @@ class BasePathDetector
      */
     public function getBasePath(): string
     {
-        // For apache
-        if ($this->phpSapi === 'apache2handler' || $this->phpSapi === 'fpm-fcgi') {
-            return $this->getBasePathFromApache($this->server);
-        }
-
-        // For built-in server
+        // The built-in server
         if ($this->phpSapi === 'cli-server') {
-            return $this->getBasePathFromBuiltIn($this->server);
+            return $this->getBasePathByScriptName($this->server);
         }
 
-        return '';
+        return $this->getBasePathByRequestUri($this->server);
     }
 
     /**
      * Return basePath for built-in server.
      *
-     * @param array<mixed> $server The SERVER data to use
+     * @param array $server The SERVER data to use
      *
      * @return string The base path
      */
-    private function getBasePathFromBuiltIn(array $server): string
+    private function getBasePathByScriptName(array $server): string
     {
-        $scriptName = $server['SCRIPT_NAME'];
+        $scriptName = (string)$server['SCRIPT_NAME'];
         $basePath = str_replace('\\', '/', dirname($scriptName));
 
         if (strlen($basePath) > 1) {
@@ -71,16 +66,34 @@ class BasePathDetector
     /**
      * Return basePath for apache server.
      *
-     * @param array<mixed> $server The SERVER data to use
+     * @param array $server The SERVER data to use
      *
      * @return string The base path
      */
-    private function getBasePathFromApache(array $server): string
+    private function getBasePathByRequestUri(array $server): string
     {
         if (!isset($server['REQUEST_URI'])) {
             return '';
         }
 
-        return dirname($server['SCRIPT_NAME']);
+        $scriptName = $server['SCRIPT_NAME'];
+
+        $basePath = (string)parse_url($server['REQUEST_URI'], PHP_URL_PATH);
+        $scriptName = str_replace('\\', '/', dirname($scriptName, 2));
+
+        if ($scriptName === '/') {
+            return '';
+        }
+
+        $length = strlen($scriptName);
+        if ($length > 0) {
+            $basePath = substr($basePath, 0, $length);
+        }
+
+        if (strlen($basePath) > 1) {
+            return $basePath;
+        }
+
+        return '';
     }
 }
